0e740123fa39c45459466e11aefd05fd23e3589b
Ian McEwen <mian@cyverse.org>
2021-02-12 11:14:19 -0700

    Version 3.0.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/kameleon "3.0.5"
+(defproject org.cyverse/kameleon "3.0.6-SNAPSHOT"
   :description "Library for interacting with backend relational databases."
   :url "https://github.com/cyverse-de/kameleon"
   :license {:name "BSD"
