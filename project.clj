8e8a5d8f4810e5a317ab00c638a7a6e9c4476aef
Sarah Roberts <sarahr@cyverse.org>
2023-05-23 15:53:59 -0700

    Version 3.0.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/kameleon "3.0.8-SNAPSHOT"
+(defproject org.cyverse/kameleon "3.0.8"
   :description "Library for interacting with backend relational databases."
   :url "https://github.com/cyverse-de/kameleon"
   :license {:name "BSD"
