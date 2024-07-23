6bba5938ee32f093513013954e4890e6346e09a1
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-23 12:29:41 -0700

    CORE-2006: updated depenencies and .gitignore

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://github.com/cyverse-de/kameleon"
   :license {:name "BSD"
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.logging "1.2.4"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [org.clojure/tools.logging "1.3.0"]
                  [clj-time "0.15.2"]
-                 [com.mchange/c3p0 "0.9.5.5"]
+                 [com.mchange/c3p0 "0.10.1"]
                  [korma "0.4.3"
                   :exclusions [c3p0]]
                  [me.raynes/fs "1.4.6"]
-                 [org.postgresql/postgresql "42.6.0"]
+                 [org.postgresql/postgresql "42.7.3"]
                  [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.8.9"]
                  [slingshot "0.12.2"]]
   :plugins [[lein-ancient "0.7.0"]
-            [lein-marginalia "0.7.1"]
-            [test2junit "1.2.2"]
-            [jonase/eastwood "0.2.3"]]
+            [lein-marginalia "0.9.2"]
+            [test2junit "1.4.4"]
+            [jonase/eastwood "1.4.3"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :eastwood {:exclude-namespaces [apps.protocols :test-paths]
